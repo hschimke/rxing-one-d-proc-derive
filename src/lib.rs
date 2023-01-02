ac25e6c2edcbbad6ac6dd819a758fe5432266708
Henry Schimke <henry@azule.info>
2023-01-02 16:30:57 -0600

    update for cach and v0.2

--- src/lib.rs
+++ src/lib.rs
@@ -25,14 +25,14 @@ fn impl_one_d_reader_macro(ast: &syn::DeriveInput) -> TokenStream {
         use crate::Reader;
 
         impl Reader for #name {
-            fn decode(&mut self, image: &crate::BinaryBitmap) -> Result<crate::RXingResult, Exceptions> {
+            fn decode(&mut self, image: &mut crate::BinaryBitmap) -> Result<crate::RXingResult, Exceptions> {
               self.decode_with_hints(image, &HashMap::new())
             }
         
             // Note that we don't try rotation without the try harder flag, even if rotation was supported.
             fn decode_with_hints(
                 &mut self,
-                image: &crate::BinaryBitmap,
+                image: &mut crate::BinaryBitmap,
                 hints: &DecodingHintDictionary,
             ) -> Result<crate::RXingResult, Exceptions> {
               if let Ok(res) = self.doDecode(image, hints) {
@@ -40,8 +40,8 @@ fn impl_one_d_reader_macro(ast: &syn::DeriveInput) -> TokenStream {
               }else {
                 let tryHarder = hints.contains_key(&DecodeHintType::TRY_HARDER);
                 if tryHarder && image.isRotateSupported() {
-                  let rotatedImage = image.rotateCounterClockwise();
-                  let mut result = self.doDecode(&rotatedImage, hints)?;
+                  let mut rotatedImage = image.rotateCounterClockwise();
+                  let mut result = self.doDecode(&mut rotatedImage, hints)?;
                   // Record that we found it rotated 90 degrees CCW / 270 degrees CW
                  let metadata = result.getRXingResultMetadata();
                   let mut orientation = 270;
